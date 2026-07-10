8f42117dc3efd900e449f64e7b64fee086fa2e3c
gk <promise_ghostking@163.com>
2026-07-10 14:53:58 +0800

    feat: 添加自动路由配置

--- packages/ai-screen-design/src/router/index.ts
+++ packages/ai-screen-design/src/router/index.ts
@@ -1,8 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import type { Component } from 'vue'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+type ViewModule = {
+  default: Component & {
+    __name: string
+  }
+}
+
+function _createRouter(): RouteRecordRaw[] {
+  const views = import.meta.glob<ViewModule>('../views/**/*.vue', { eager: true })
+
+  return Object.entries(views).map(([path, component]: [path: string, component: ViewModule]) => {
+    const _component = component.default
+
+    return {
+      name: _component.name ?? _component?.__name,
+      path: `/${_component.name ?? _component?.__name}`,
+      component: () => import(path),
+    }
+  })
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: _createRouter(),
 })
 
 export default router
---
+++ packages/ai-screen-design/src/views/ScreenEditor.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'ScreenEditor',
+})
+</script>
+
+<template>
+  <div>screen editor</div>
+</template>
+
+<style scoped lang="scss"></style>
